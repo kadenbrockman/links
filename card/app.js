af7adc43555d291f14593f3babdd7b2eda3392b7
kadenbrockman <79776914+kadenbrockman@users.noreply.github.com>
2021-03-28 17:21:13 -0700

    Add files via upload

---
+++ card/app.js
@@ -0,0 +1,38 @@
+var card = document.getElementById('cardz');
+
+document.getElementById('flip').addEventListener('click', function() {
+    card.classList.toggle('flipped');
+}, false);
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+function Copy() {
+  var Url = document.getElementById("url");
+  Url.innerHTML = window.location.href;
+  console.log(Url.innerHTML)
+  Url.select();
+  document.execCommand("copy");
+}
