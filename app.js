a5f915d749ae898a02139ab771d74bc5c7492972
kadenbrockman <79776914+kadenbrockman@users.noreply.github.com>
2021-05-17 08:59:48 -0700

    updating may 17

--- app.js
+++
@@ -1,5 +0,0 @@
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.25 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo("#profileinfo", { opacity: 0 }, { opacity: 1, duration: .5 }, "-=.5");
-tl.fromTo("#links", { opacity: 0 }, { opacity: 1, duration: .5, delay: -.35 });
